df1707eafa76297bf1b20c07a8304dc11cd55315
Dustin Klinkert <dustin.klinkert.kl2@fh-zwickau.de>
2021-11-06 14:01:18 +0100

    TestMethode testListAllGrades erstellt

--- src/test/java/whz/pti/eva/praktikum_02/grade/GradeControllerTest.java
+++ src/test/java/whz/pti/eva/praktikum_02/grade/GradeControllerTest.java
@@ -1,10 +1,15 @@
 package whz.pti.eva.praktikum_02.grade;
 
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,7 +33,12 @@ public class GradeControllerTest {
     }
 
     @Test
-    public void testListAllGrades() {
-    	
+    public void testListAllGrades() throws Exception {
+        mockMvc.perform(get("/grades")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .param("grade","1")
+                .param("lecture", "Mathe"))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 }
